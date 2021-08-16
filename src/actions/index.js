be63b0ee3332068b264207e41ce97dc98a118e5b
elfrmkr <elfrmkr98@gmail.com>
2021-08-16 16:59:08 +0300

    linking thet buttons if the user is the creator of that stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ import {
   FETCH_STREAM,
   FETCH_STREAMS,
   EDIT_STREAM,
-  DELETE_STREAM
+  DELETE_STREAM,
 } from "./types";
 
 export const signIn = (userId) => {
@@ -22,33 +22,33 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
-export const fetchStreams = () => async dispatch => {
-  const response = await streams.get('/streams');
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
 
-  dispatch({ type: FETCH_STREAMS, payload: response.data});
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
-export const fetchStream = (id) => async dispatch => {
+export const fetchStream = (id) => async (dispatch) => {
   const response = await streams.get(`/streams/${id}`);
 
-  dispatch({ type: FETCH_STREAM, payload: response.data});
+  dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
-export const editStream = (id, formValues) => async dispatch => {
+export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
-  dispatch({ type: EDIT_STREAM, payload: response.data});
+  dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
-export const deleteStream = (id) => async dispatch => {
-   await streams.delete(`/streams/${id}`);
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
 
-  dispatch({ type: DELETE_STREAM, payload: id});
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
-
